e66c109c03e30ddb52f01a5204aeabb1f8398231
Anna <Anna@Annas-Air.attlocal.net>
2023-07-17 21:08:56 -0500

    completed part 1 exercise

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books by Brandon Sanderson:";
+   let book1 = "https://m.media-amazon.com/images/I/81dM5-PSE3L._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51kyByMUf+L.jpg";
+   let book3 = "https://b1512865.smushcdn.com/1512865/wp-content/uploads/2019/11/Way-of-Kings.jpg?lossy=1&strip=1&webp=1";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Mistborn" />
+         <img src={book2} alt="Warbreaker" />
+         <img src={book3} alt="The Stormlight Archive" />
       </div>      
    );
 }
---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,7 @@
+export default function HobbyLinks() {
+    let hobbyLinks = ["https://www.brandonsanderson.com/", "https://www.tamora-pierce.net/"]
+    return (
+        <a href = {hobbyLinks[0]}>Brandon Sanderson</a>,
+        <a href = {hobbyLinks[1]}>Tamora Pierce</a>
+    );
+}
